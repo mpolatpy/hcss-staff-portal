b3ea6c9d3fa141b291ebf66a95a81156b6ab3111
Murat Polat <mpolatpy@gmail.com>
2021-08-09 00:42:40 -0400

    redirect issue-bug fixed

--- server.js
+++ server.js
@@ -17,18 +17,6 @@ app.use(express.urlencoded({ extended: true }));
 // app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
-
-    // Redirect all traffic to https
-    app.all('*', function(req, res, next){
-        console.log('req start: ',req.secure, req.hostname, req.url, app.get('port'));
-        
-        if (req.secure) {
-            return next();
-        }
-    
-        res.redirect('https://'+req.hostname + ':' + app.get('secPort') + req.url);
-    });
-
     app.use(express.static(path.join(__dirname, 'client/build')));
 
     app.get('*', function (req, res) {
